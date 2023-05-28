99578d28967982b4de21b5aedc9f449ae26034b5
nellysme <nelly1@live.no>
2023-05-28 22:08:24 +0200

    ee
    eee

--- js/index.js
+++ js/index.js
@@ -3,6 +3,13 @@ const postsContainer = document.querySelector('.latest-posts');
 const specificId = 38;
 const loadingSpinner = document.querySelector('.loading-circle');
 
+const imageSliderContainer = document.querySelector('.image-slider-container');
+const images = imageSliderContainer.querySelectorAll('img');
+
+
+
+
+
 loadingSpinner.style.display = 'block'; 
 
 fetch(url + specificId)
@@ -19,7 +26,43 @@ fetch(url + specificId)
 
     loadingSpinner.style.display = 'none'; 
 
-    
 
+
+
+
+
+const imageSliderContainer = document.querySelector('.image-slider-container');
+const images = imageSliderContainer.querySelectorAll('img');
+
+let currentIndex = 0;
+const slideWidth = images[0].clientWidth * 2;
+const totalSlides = images.length;
+
+function goToSlide(index) {
+  imageSliderContainer.style.transform = `translateX(-${index * slideWidth}px)`;
+  currentIndex = index;
+}
+
+function nextSlide() {
+  if (currentIndex === totalSlides - 2) { 
+    goToSlide(0);
+  } else {
+    goToSlide(currentIndex + 1);
+  }
+}
+
+function previousSlide() {
+  if (currentIndex === 0) {
+    goToSlide(totalSlides - 2);
+  } else {
+    goToSlide(currentIndex - 1);
+  }
+}
+
+document.querySelector('.next-button').addEventListener('click', nextSlide);
+document.querySelector('.previous-button').addEventListener('click', previousSlide);
+
+
+    
   });
   
